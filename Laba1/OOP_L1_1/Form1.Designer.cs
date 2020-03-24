c454a12e000b4bb0789f3f528f55f1a7c4c6f3d2
BruiR <41452848+BruiR@users.noreply.github.com>
2020-03-25 01:46:16 +0300

    Add files via upload

---
+++ Laba1/OOP_L1_1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace OOP_L1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDraw = new System.Windows.Forms.Button();
+            this.pBoxDrawing = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDrawing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnDraw
+            // 
+            this.BtnDraw.Location = new System.Drawing.Point(12, 10);
+            this.BtnDraw.Name = "BtnDraw";
+            this.BtnDraw.Size = new System.Drawing.Size(80, 25);
+            this.BtnDraw.TabIndex = 0;
+            this.BtnDraw.Text = "Нарисовать";
+            this.BtnDraw.UseVisualStyleBackColor = true;
+            this.BtnDraw.Click += new System.EventHandler(this.BtnDraw_Click);
+            // 
+            // pBoxDrawing
+            // 
+            this.pBoxDrawing.Location = new System.Drawing.Point(105, 12);
+            this.pBoxDrawing.Name = "pBoxDrawing";
+            this.pBoxDrawing.Size = new System.Drawing.Size(634, 277);
+            this.pBoxDrawing.TabIndex = 2;
+            this.pBoxDrawing.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pBoxDrawing);
+            this.Controls.Add(this.BtnDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxDrawing)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDraw;
+        private System.Windows.Forms.PictureBox pBoxDrawing;
+    }
+}
+
---
+++ Laba1/OOP_L1_1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_L1_1
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphic;
+        private readonly List<Shape> FiguresList =new List<Shape>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphic = pBoxDrawing.CreateGraphics();
+            FiguresList.Add(new Line(new Pen(Color.Blue, 6), new Point(10, 10), new Point(10, 100)));
+            FiguresList.Add(new Line(new Pen(Color.Blue, 4), new Point(10, 10), new Point(100, 100)));
+            FiguresList.Add(new Line(new Pen(Color.Black, 5), new Point(100, 100), new Point(200, 100)));
+            FiguresList.Add(new Line(new Pen(Color.Green, 15), new Point(100, 100), new Point(200, 140)));
+        }
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            foreach (Shape Figure in FiguresList)
+            {
+                Figure.Draw(graphic);
+            }
+        }      
+    }
+}
