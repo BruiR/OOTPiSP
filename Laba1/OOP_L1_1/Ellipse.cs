4474ceb19bc3d319f7047258f40a081e88578155
BruiR <bruroma1945@gmail.com>
2020-04-05 23:39:02 +0300

    v1_4
    Real added. +Circle,Ellipse,Rectangle,Square

---
+++ Laba1/OOP_L1_1/Ellipse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace OOP_L1_1
+{
+    class Ellipse : Rectangular
+    {
+        public Ellipse(Pen PenColor, Point FirstPoint, Point SecondPoint) : base(PenColor, FirstPoint, SecondPoint) { }
+ /*       public int Width
+        {
+            get
+            {
+                return Math.Abs(FirstPoint.X - SecondPoint.X);
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return Math.Abs(FirstPoint.Y - SecondPoint.Y);
+            }
+        } */
+        public override void Draw(Graphics graphic)
+        {
+            graphic.DrawEllipse(PenColor, FirstPoint.X, FirstPoint.Y, Width, Height);
+        }
+    }
+}
---
+++ Laba1/OOP_L1_1/Rectangular.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace OOP_L1_1
+{
+    abstract class Rectangular : Shape
+    {
+        public abstract override void Draw(Graphics graphic);
+        public Rectangular(Pen PenColor, Point FirstPoint, Point SecondPoint) : base(PenColor, FirstPoint, SecondPoint) { }
+        public int Width
+        {
+            get
+            {
+                return Math.Abs(FirstPoint.X - SecondPoint.X);
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return Math.Abs(FirstPoint.Y - SecondPoint.Y);
+            }
+        }
+    }
+}
